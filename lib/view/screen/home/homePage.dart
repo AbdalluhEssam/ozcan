71d36b36ce2f4ca811fe17d51f2096c75cb6425c
abdal <abdallhesam100@gmail.com>
2023-12-09 19:41:21 +0200

    new inti

--- lib/view/screen/home/homePage.dart
+++ lib/view/screen/home/homePage.dart
@@ -1,6 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 import 'package:ozcan/likeapi.dart';
 import 'package:ozcan/view/screen/home/searchscreen.dart';
